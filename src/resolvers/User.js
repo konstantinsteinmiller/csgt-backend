39aea5f78eedc90d662d0a352cd2f38d9dc1586b
Eternal <konstantinsteinmiller@gmail.com>
2019-06-11 21:27:03 +0200

    dont show email on query

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,13 +1,13 @@
 import getUserId from '../utils/getUserId'
 
 const User = {
-  // email: {
-  //   fragment: 'fragment userId on User { id }',
-  //   resolve(parent, args, { request }) {
-  //     const userId = getUserId(request, false)
-  //     return userId && userId === parent.id ? parent.email : null
-  //   }
-  // },
+  email: {
+    fragment: 'fragment userId on User { id }',
+    resolve(parent, args, { request }) {
+      const userId = getUserId(request, false)
+      return userId && userId === parent.id ? parent.email : null
+    }
+  },
   password: {
     fragment: 'fragment userId on User { id }',
     resolve(parent, args, { request }) {
